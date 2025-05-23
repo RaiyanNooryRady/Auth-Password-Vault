be004e5144f4e2497bd1af6feeb3cbc8f21d9dec
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-23 23:15:13 +0600

    register & login

---
+++ auth-password-vault.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Auth Password Vault
+ * Description: A plugin to store and manage user passwords securely.
+ * Version: 1.0.0
+ * Author: Raiyan Noory Rady
+ * Author URI: https://github.com/RaiyanNooryRady
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: auth-password-vault
+ */
+
+
+
+?>
